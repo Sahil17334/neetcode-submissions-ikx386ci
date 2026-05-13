d95aea36334148f196cc78d7082f73bab7f0cf0e
Sahil17334 <115864834+Sahil17334@users.noreply.github.com>
2026-05-13 16:29:49 +0530

    Add: merge-k-sorted-linked-lists - submission-5

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-5.py
@@ -0,0 +1,34 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:    
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if not lists:
+            return None
+        n = len(lists)
+        for i in range(1, n):
+            lists[i] = self.merge(lists[i-1], lists[i])
+        
+        return lists[-1]
+
+    
+    def merge(self, l1, l2):
+        dummy = ListNode(0)
+        cur = dummy
+        while l1 and l2:
+            if l1.val < l2.val:
+                cur.next = l1
+                l1 = l1.next
+            else:
+                cur.next = l2
+                l2 = l2.next
+            cur = cur.next
+        
+        if l1:
+            cur.next = l1
+        if l2:
+            cur.next = l2
+        return dummy.next
